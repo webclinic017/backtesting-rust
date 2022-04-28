08c7c1dcfdeeacae9be940bd09ba90cc5715482b
mbroo <mbrooksr@gmail.com>
2022-04-28 10:58:01 -0400

    Small refactors

--- src/strategy.rs
+++ src/strategy.rs
@@ -3,7 +3,8 @@ use chrono::{Duration, NaiveDate, NaiveDateTime, NaiveTime};
 use bdays::HolidayCalendar;
 
 pub const N_FIELDS: usize = 7;
-pub static FIELD_NAMES: [&str; N_FIELDS] = ["interval", "start time", "end time", "sharpe", "max drawup", "max drawdown", "n obs"];
+pub static FIELD_NAMES: [&str; N_FIELDS] = ["interval", "start time", "end time", "sharpe",
+                                            "max drawup", "max drawdown", "n obs"];
 pub struct StrategyResult {
     pub interval: u64,
     pub start_time: NaiveTime,
@@ -17,6 +18,7 @@ pub struct StrategyResult {
 }
 impl StrategyResult {
     pub fn fields_to_strings(&self) -> [String; N_FIELDS] {
+        // Doesn't include datetime or value data yet
         [self.interval.to_string(), self.start_time.to_string(), self.end_time.to_string(),
             self.sharpe.to_string(), self.max_drawup.to_string(), self.max_drawdown.to_string(),
             self.n_obs.to_string()]
